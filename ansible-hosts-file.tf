ed8c38e04ed003f44c7d8b9b4b255bc3b96c9d37
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2024-03-05 17:08:58 -0600

    fix: add connect to hosts, add try

--- ansible-hosts-file.tf
+++ ansible-hosts-file.tf
@@ -1,14 +1,14 @@
 resource "local_file" "hosts_ini" {
   content = templatefile("${path.module}/hosts.ini.tpl", {
     cloud_storage_region       = var.aws_region
-    client_public_ips          = aws_instance.client[*].public_ip
-    client_private_ips         = aws_instance.client[*].private_ip
+    client_public_ips          = try(aws_instance.client[*].public_ip)
+    client_private_ips         = try(aws_instance.client[*].private_ip)
     enable_monitoring          = var.enable_monitoring
     enable_connect             = var.enable_connect
-    monitor_public_ip          = var.enable_monitoring ? aws_instance.prometheus[0].public_ip : ""
-    monitor_private_ip         = var.enable_monitoring ? aws_instance.prometheus[0].private_ip : ""
-    connect_public_ip          = var.enable_connect ? aws_instance.connect[0].public_ip : ""
-    connect_private_ip         = var.enable_connect ? aws_instance.connect[0].private_ip : ""
+    monitor_public_ip          = try(var.enable_monitoring ? aws_instance.prometheus[0].public_ip : "")
+    monitor_private_ip         = try(var.enable_monitoring ? aws_instance.prometheus[0].private_ip : "")
+    connect_public_ips         = try(var.enable_connect ? aws_instance.connect[0].public_ip : "")
+    connect_private_ips        = try(var.enable_connect ? aws_instance.connect[0].private_ip : "")
     rack                       = var.ha ? aws_instance.broker[*].placement_partition_number : aws_instance.broker[*].availability_zone
     rack_awareness             = var.ha || length(var.availability_zone) > 1
     availability_zone          = aws_instance.broker[*].availability_zone
