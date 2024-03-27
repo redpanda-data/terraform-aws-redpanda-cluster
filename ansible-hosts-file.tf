d6740502e1335562b30633f8910f98ea022c85cc
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2024-03-27 17:46:09 -0500

    fix: make connect iterable in hosts file (#17)

--- ansible-hosts-file.tf
+++ ansible-hosts-file.tf
@@ -7,8 +7,8 @@ resource "local_file" "hosts_ini" {
     enable_connect             = var.enable_connect
     monitor_public_ip          = try(var.enable_monitoring ? aws_instance.prometheus[0].public_ip : "")
     monitor_private_ip         = try(var.enable_monitoring ? aws_instance.prometheus[0].private_ip : "")
-    connect_public_ips         = try(var.enable_connect ? aws_instance.connect[0].public_ip : "")
-    connect_private_ips        = try(var.enable_connect ? aws_instance.connect[0].private_ip : "")
+    connect_public_ips         = try(var.enable_connect ? aws_instance.connect[*].public_ip : [])
+    connect_private_ips        = try(var.enable_connect ? aws_instance.connect[*].private_ip : [])
     rack                       = var.ha ? aws_instance.broker[*].placement_partition_number : aws_instance.broker[*].availability_zone
     rack_awareness             = var.ha || length(var.availability_zone) > 1
     availability_zone          = aws_instance.broker[*].availability_zone
