7fde4e8d26de78cf30bd43bf59fd10112507c6eb
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2024-10-31 17:06:54 -0500

    test

--- client.tf
+++ client.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "client" {
-  count                       = var.client_count  * length(var.availability_zone)
-  ami                         = coalesce(var.client_ami, data.aws_ami.ami.image_id)
+  count                       = var.client_count
+  ami = coalesce(var.client_ami, data.aws_ami.ami.image_id)
   instance_type               = var.client_instance_type
   key_name                    = aws_key_pair.ssh.key_name
-  subnet_id                   = try(lookup(local.merged_subnets["client"], var.availability_zone[count.index % length(var.availability_zone)]), "")
-  vpc_security_group_ids      = coalesce(var.security_groups_client, [aws_security_group.node_sec_group.id])
+  subnet_id = try(lookup(local.merged_subnets["client"], var.availability_zone[count.index % length(var.availability_zone)]), "")
+  vpc_security_group_ids = coalesce(var.security_groups_client, [aws_security_group.node_sec_group.id])
   associate_public_ip_address = var.associate_public_ip_addr_client
 
   tags = merge(
--- prometheus.tf
+++ prometheus.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "prometheus" {
-  count                       = var.enable_monitoring ? 1 * length(var.availability_zone) : 0
-  ami                         = coalesce(var.prometheus_ami, data.aws_ami.ami.image_id)
+  count                       = var.enable_monitoring ? 1 : 0
+  ami = coalesce(var.prometheus_ami, data.aws_ami.ami.image_id)
   instance_type               = var.prometheus_instance_type
   key_name                    = aws_key_pair.ssh.key_name
-  subnet_id                   = try(lookup(local.merged_subnets["monitor"], var.availability_zone[count.index % length(var.availability_zone)]), "")
-  vpc_security_group_ids      = coalesce(var.security_groups_prometheus, [aws_security_group.node_sec_group.id])
+  subnet_id = try(lookup(local.merged_subnets["monitor"], var.availability_zone[count.index % length(var.availability_zone)]), "")
+  vpc_security_group_ids = coalesce(var.security_groups_prometheus, [aws_security_group.node_sec_group.id])
   associate_public_ip_address = var.associate_public_ip_addr
+  availability_zone           = "${var.availability_zone[count.index % length(var.availability_zone)]}"
 
   tags = merge(
     local.merged_tags,
@@ -15,8 +16,8 @@ resource "aws_instance" "prometheus" {
   )
 
   connection {
-    user        = var.distro_ssh_user[var.distro]
-    host        = self.public_ip
+    user = var.distro_ssh_user[var.distro]
+    host = self.public_ip
     private_key = file(var.private_key_path)
   }
 
